f2a739c56b80d80d845eab3e4e6e54dabfbcb5e6
DevM7mdAli <74423891+DevM7mdAli@users.noreply.github.com>
2025-11-26 22:01:38 +0300

    fix: updaing outdated list of skills

--- src/components/Skills/ListOfSkills.tsx
+++ src/components/Skills/ListOfSkills.tsx
@@ -5,11 +5,20 @@ import {
   SiSqlite,
   SiDart,
   SiTypescript,
+  SiPython,
+  SiAngular,
+  SiNestjs,
+  SiStrapi,
+  SiExpress,
+  SiFastapi,
+  SiPostgresql,
+  SiMongodb,
+  SiFirebase,
 } from "react-icons/si";
 import { TbBrandReactNative } from "react-icons/tb";
 import { FaReact, FaPhp } from "react-icons/fa";
 import { RiFlutterFill } from "react-icons/ri";
-import { IoLogoFirebase, IoLogoJavascript } from "react-icons/io5";
+import { IoLogoJavascript } from "react-icons/io5";
 import { MotionDiv } from "../../utils/motion";
 import { useTranslation } from "react-i18next";
 
@@ -23,30 +32,38 @@ export default function ListOfSkills() {
         { name: "JavaScript", icon: IoLogoJavascript },
         { name: "TypeScript", icon: SiTypescript },
         { name: "Dart", icon: SiDart },
+        { name: "python", icon: SiPython },
+        { name: "PHP", icon: FaPhp },
       ],
     },
     {
       title: "Front End",
       allSkill: [
         { name: "React JS", icon: FaReact },
-        { name: "Fluter", icon: RiFlutterFill },
+        { name: "Angular", icon: SiAngular },
         { name: "React Native", icon: TbBrandReactNative },
+        { name: "NextJS", icon: SiNextdotjs },
+        { name: "Fluter", icon: RiFlutterFill },
       ],
     },
     {
       title: "Back End",
       allSkill: [
-        { name: "PHP", icon: FaPhp },
-        { name: "NextJS", icon: SiNextdotjs },
+        { name: "FastAPI", icon: SiFastapi },
+        { name: "Express JS", icon: SiExpress },
+        { name: "NestJS", icon: SiNestjs },
+        { name: "Strapi", icon: SiStrapi },
         { name: "Flask", icon: SiFlask },
       ],
     },
     {
       title: "DataBase",
       allSkill: [
-        { name: "FireStore", icon: IoLogoFirebase },
+        { name: "FireStore", icon: SiFirebase },
         { name: "mySQL", icon: SiMysql },
         { name: "SQLite", icon: SiSqlite },
+        { name: "MongoDB", icon: SiMongodb },
+        { name: "PostgreSQL", icon: SiPostgresql },
       ],
     },
   ];
